69c02dd634e33e0ec46b8d1e764b5c32c0d6860d
Tomáš Konrády <tomaskonrady@gmail.com>
2021-04-08 11:53:26 +0200

    feat: spectrum

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "recoil";
 
 import "./App.css";
+import { Button, Flex, View } from "@adobe/react-spectrum";
 
 const App = () => {
-  return <div className="App" />;
+  return (
+    <main className="App">
+      <Flex direction="column" gap="size-100" alignItems="center">
+        <Button variant="cta" onPress={() => alert("Hey there!")}>
+          Hello React Spectrum!
+        </Button>
+        <Button onPress={() => alert("Hey there!")}>
+          Hello React Spectrum!
+        </Button>
+      </Flex>
+    </main>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React, { StrictMode } from "react";
 import { render } from "react-dom";
+import { Provider, defaultTheme } from "@adobe/react-spectrum";
 import App from "./App";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 
 render(
   <StrictMode>
-    <App />
+    <Provider theme={defaultTheme}>
+      <App />
+    </Provider>
   </StrictMode>,
   document.getElementById("root")
 );
-
-reportWebVitals(console.log);
