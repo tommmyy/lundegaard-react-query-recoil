afd7f0181ff5011d49450b7acbcb2157e053ff08
Tomáš Konrády <tomaskonrady@gmail.com>
2021-04-09 10:14:57 +0200

    feat: react-query - ex7

--- src/Ex7ReactQueryParams.js
+++ src/Ex7ReactQueryParams.js
@@ -1,37 +1,62 @@
-import { Flex, Text, Button } from "@adobe/react-spectrum";
+// queryKey as an array
+// show caching
+
+import { Flex, Text, Button, Provider } from "@adobe/react-spectrum";
 import { useQuery } from "react-query";
 import axios from "axios";
 import { useState } from "react";
 import { Column } from "./helpers";
 
-const Pokemon = () => {
-  const { isLoading, error, data } = useQuery("pokemon", () => {
-    return axios
-      .get("https://pokeapi.co/api/v2/pokemon/")
-      .then(response => response.data.results);
-  });
+const LIMIT = 20;
 
-  if (isLoading) return "Loading...";
+const Pokemon = () => {
+  const [page, setPage] = useState(1);
+
+  const { isLoading, isFetching, error, data } = useQuery(
+    ["pokemon", page],
+    async () => {
+      // const { isLoading, error, data } = useQuery("pokemon", async () => {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+
+      return axios
+        .get(
+          `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}&offset=${(page -
+            1) *
+            LIMIT}`
+        )
+        .then(response => response.data);
+    }
+  );
 
   if (error) return "An error has occurred: " + error.message;
 
   return (
     <Column>
-      {data.map(({ name }) => (
-        <Text key={name}>{name}</Text>
-      ))}
+      {data?.results.map(({ name }) => <Text key={name}>{name}</Text>)}
+
+      <Provider isDisabled={!!isLoading}>
+        <Flex gap="size-100">
+          <Button
+            isDisabled={!data?.previous}
+            onPress={() => setPage(page - 1)}
+          >
+            Previous
+          </Button>
+
+          <Button isDisabled={!data?.next} onPress={() => setPage(page + 1)}>
+            Next
+          </Button>
+        </Flex>
+      </Provider>
+      {isLoading ? <Text>Loading...</Text> : null}
+      {isFetching ? <Text>Fetching...</Text> : null}
     </Column>
   );
 };
 const Ex7ReactQueryParams = () => {
-  const [isOpen, setIsOpen] = useState(true);
-
   return (
     <Column>
-      <Button onPress={() => setIsOpen(!isOpen)} maxWidth="size-100">
-        {isOpen ? "Close" : "Open"}
-      </Button>
-      <Flex>{isOpen && <Pokemon />}</Flex>
+      <Pokemon />
     </Column>
   );
 };
